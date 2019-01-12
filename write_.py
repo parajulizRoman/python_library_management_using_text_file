788b749f541930a63e8ac4f14d208c56c8ff2308
Roman Parajuli <notorious.garage@gmail.com>
2019-01-12 22:58:14 +0545

    Fixed the BUG
    This part was making my code buggy. 
    I had commented a few lines of code, as I forgot its functionality. Then I executed the code and got errors like not finding user selection form booklist, not being able to perform borrow process

--- write_.py
+++ write_.py
@@ -1,15 +1,15 @@
 
-
-'''with open('book_details.txt','r') as mf:
+with open('book_details.txt','r') as mf:
     book_list = mf.read()
     lis = []
     lis.append(book_list)
     #print(lis)
-'''
+
 
 def content_to_write(contents):
     with open('book_details.txt','w') as mf:
-        book_contents = str(contents).replace(']','')
+        book_contents = (contents)
         #print(book_contents)
-        mf.write(book_contents)
+        mf.write(str(book_contents).replace(']','').replace('[',''))
+
 
