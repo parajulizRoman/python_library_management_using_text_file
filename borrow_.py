b1c4c31515d4d970a30332823301e770364dae06
Roman Parajuli <notorious.garage@gmail.com>
2019-01-13 21:34:49 +0545

    Updated borrow Process
    reduced the contents of index[i][2] by 1 after the book borrowing confirmation form user

--- borrow_.py
+++ borrow_.py
@@ -2,6 +2,8 @@ from write_ import*
 from read_ import*
 
 def borrow_procress_(userSelection,contents,i):
-        contents[i][2] = str(int(contents[i][2]) - 1 )
+        contents[i][2] = int(contents[i][2])
+        contents[i][2] -= 1
         return contents, True
+
 
