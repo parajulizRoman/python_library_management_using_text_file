d3e0fe15ab35404de4a449b951a1ae7d8e917648
Roman Parajuli <notorious.garage@gmail.com>
2019-01-14 19:16:50 +0545

    Moved the code
    I've moved the code from here to write_py file

--- msg_.py
+++ msg_.py
@@ -1,14 +1,1 @@
-from datetime import datetime
-
-timeStamp = str(datetime.now())
-
-
-def borrow_message_for_user(userName,userSelection):
-    with open(userName +'.txt','w') as mf:
-        message = userName+' BORROWED '+userSelection+' AT '+ timeStamp
-        mf.write(message)
-
-def return_message_for_user(userName,userSelection):
-    with open(userName +'.txt','a') as mf:
-        message = userName+' RETURNED '+userSelection+' AT '+ timeStamp
-        mf.write(message)
+ 
