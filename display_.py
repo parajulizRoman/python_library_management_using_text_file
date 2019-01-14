2fdc35a3e8e4af74634bf8c6fd7efdee4f8403f5
Roman Parajuli <notorious.garage@gmail.com>
2019-01-14 19:15:57 +0545

    updated the display program
    Added code to replace ' by a white_space

--- display_.py
+++ display_.py
@@ -2,7 +2,7 @@ def start_screen():
     with open('book_details.txt','r') as mf:
         print('BOOKS AVAILABLE TO BORROW\n')
         file_contents = mf.read()
-        file_contents = file_contents.replace('[','').replace(']','')
+        file_contents = file_contents.replace('[','').replace(']','').replace("'","")
         #print(type(file_contents))
         print(file_contents)
     
